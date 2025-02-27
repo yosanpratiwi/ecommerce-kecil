4a09568c1c03737213348e2b6cf14f518a1aa403
yosanpratiwi <you@10231091@student.itk.ac.id>
2025-02-27 14:26:01 +0800

    Add basic Express.js setup and routes

---
+++ raisha_backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+app.use(cors());
+app.use(express.json());
+app.get('/'
+, (req, res) => {
+res.send('Hello World from Express.js!');
+});
+app.post('/data'
+, (req, res) => {
+const { nama } = req.body;
+res.send('Data diterima: ${nama}');
+});
+app.listen(PORT, () => {
+console.log('Server berjalan di http://localhost:${PORT}');
+});
